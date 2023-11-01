0838a602f564c695107b475b00e7ded28cb2e976
Danil Anikin <mart4r.clearlist@gmail.com>
2023-11-01 14:58:33 +1000

    Added createColumnsDifference

--- Laboratory - 1/main.py
+++ Laboratory - 1/main.py
@@ -13,4 +13,31 @@ def findColumsForDelete(data) -> []:
     return columnsDelete
 
 dataFrame = dataFrame.drop(columns = findColumsForDelete(dataFrame))
-print(dataFrame)
+
+# Second Part
+
+def renameColumns(data, oldName, newName):
+    return data.rename(columns={oldName: newName})
+
+dataFrame = renameColumns(dataFrame, "Остаток", "ВсегоТовара")
+dataFrame = renameColumns(dataFrame, "Количество", "НаВитрине")
+
+# можно было написать все одной строчкой: dataFrame = dataFrame.rename(columns={"Остаток": "ВсегоТовара", "Остаток": "ВсегоТовара" })
+
+def avgPricesCategory(data):
+    groupPrice = data.groupby("КодКатегории")["Цена"].apply(list)
+    
+    for index, item in enumerate(groupPrice):
+       groupPrice[index] = (round(sum(item) / len(item), 3)) # округляю до трех знаков после запятой, чтобы приятнее выглядило 
+    
+    return groupPrice
+
+def findDifference(row, group):
+    return row["Цена"] - group[row["КодКатегории"]]
+
+def createColumnsDifference(data):
+    group = avgPricesCategory(data)
+    data["Разница"] = data.apply(lambda row: findDifference(row, group), axis=1)
+    print(data)
+
+createColumnsDifference(dataFrame)
